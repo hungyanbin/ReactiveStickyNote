f5f4ca67632cd49a44bc7a3670bb4b2261468cc4
hungyanbin <hung_yanbin@hotmail.com>
2022-06-22 22:26:31 +0800

    Update gradle dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
 
     dependencies {
-        classpath ("com.google.gms:google-services:4.3.8")
-        classpath ("com.android.tools.build:gradle:7.1.0-alpha05")
+        classpath ("com.google.gms:google-services:4.3.10")
+        classpath ("com.android.tools.build:gradle:7.1.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-    implementation("com.android.tools.build:gradle:7.1.0-alpha03")
+    implementation("com.android.tools.build:gradle:7.1.2")
 }
